6933c3ad00681d6745dec73c64b7be20c3b98457
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-04-19 10:50:18 -0300

    feat: add splash page

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,12 @@
+import 'package:dev_quiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "dev_quiz",
+      home: SplashPage(),
+    );
+  }
+}
